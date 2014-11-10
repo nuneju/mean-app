f004ca427881fab38c73995db429868c685cc32a
Juan Nunez <nunezj2@gmail.com>
2014-11-10 13:04:48 -0500

    Remove compass. Bourbon will be used instead.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,22 +1,7 @@
 module.exports = function(grunt) {
 	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
-		compass: {
-			dist: {
-				options: {
-					sassDir: 'sass',
-					cssDir: 'stylesheets'
-				}
-			}
-		},
-		watch: {
-			css: {
-				files: '**/*.scss',
-				tasks: ['compass']
-			}
-		}
+		pkg: grunt.file.readJSON('package.json')
 	});
-	grunt.loadNpmTasks('grunt-contrib-compass');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.registerTask('default',['watch']);
 }
